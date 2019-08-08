5296b26c1749a2867e9bec99e23094f194d516cc
Igor Shklyar <igor.shklyar@gmail.com>
2019-08-08 10:57:59 +0300

    Update variables and expressions section

--- my_section/001_FirstSteps/002__Variables_Datatypes_Operators/Hello.java
+++ my_section/001_FirstSteps/002__Variables_Datatypes_Operators/Hello.java
@@ -6,10 +6,19 @@ public class Hello {
 
         System.out.println("Hello World!");
 
+
+        // Variables
+//        int myFirstNumber = 5;
+//        System.out.println("myFirstNumber"); // String Literal
+//        System.out.println(myFirstNumber);
+
+
+        // Operators
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
 
+        // Expression
+        int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         int myLastOne = 1000 - myTotal;
