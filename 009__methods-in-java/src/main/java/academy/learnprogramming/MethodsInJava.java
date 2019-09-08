b50e5b50ade875e985e485dda49dc4bbe31036fe
Ihor Shkliar <igor.shklyar@gmail.com>
2019-09-08 16:22:02 +0200

    Added print method

--- 009__methods-in-java/src/main/java/academy/learnprogramming/MethodsInJava.java
+++ 009__methods-in-java/src/main/java/academy/learnprogramming/MethodsInJava.java
@@ -13,21 +13,22 @@ public class MethodsInJava {
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
-
+        int finalScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        print("Your returned final score value was", finalScore);
 
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
-
         return -1;
+    }
 
+    public static void print(String text, int score) {
+        System.out.println(text + " " + score);
     }
 }
