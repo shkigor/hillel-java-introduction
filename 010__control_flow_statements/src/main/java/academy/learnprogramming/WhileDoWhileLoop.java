4ef76ec99ca7080dd3b19f56abf491002c5eb585
Igor Shklyar <igor.shklyar@gmail.com>
2019-09-26 17:37:11 +0300

    Modify WhileDoWhileLoop

--- 010__control_flow_statements/src/main/java/academy/learnprogramming/WhileDoWhileLoop.java
+++ 010__control_flow_statements/src/main/java/academy/learnprogramming/WhileDoWhileLoop.java
@@ -3,13 +3,13 @@ package academy.learnprogramming;
 public class WhileDoWhileLoop {
 
     public static void main(String[] args) {
-        int count = 0;
-        while (count != 5) {
+        int count = 1;
+        while (count != 6) {
             System.out.println("Count value is " + count);
             count++;
         }
 
-        for (int i = 6; i != 6; i++) {
+        for (count = 1; count != 6; count++) {
             System.out.println("Count value is " + count);
         }
 
@@ -24,34 +24,49 @@ public class WhileDoWhileLoop {
 
         } while (count != 6);
 
-//        int number = 5;
-//        int finishNumber = 20;
-//        while(number <= finishNumber) {
-//            if(!isEvenNumber(number)) {
-//                number++;
-//                continue;
-//            }
-//
-//            System.out.println("Even number " + number);
-//            number++;
-//        }
+
+        /*
+            Объявите две переменные типа int - startNumber и finishNumber
+            Используя цикл while выведите все четные числа из диапазона от startNumber до finishNumber
+        */
+
+
+
+        int startNumber = 5;
+        int finishNumber = 20;
+        while(startNumber <= finishNumber) {
+            if(!isEvenNumber(startNumber)) {
+                startNumber++;
+                continue;
+            }
+
+            System.out.println("Even number " + startNumber);
+            startNumber++;
+        }
+
+
+
+
+
 
         // Modify the while code above
         // Make it also record the total number of even numbers it has found
         // and break once 5 are found
         // and at the end, display the total number of even numbers found
 
-        int number = 5;
-        int finishNumber = 20;
+
+
+        startNumber = 5;
+        finishNumber = 20;
         int evenNumbersFound = 0;
-        while (number <= finishNumber) {
-            if (!isEvenNumber(number)) {
-                number++;
+        while (startNumber <= finishNumber) {
+            if (!isEvenNumber(startNumber)) {
+                startNumber++;
                 continue;
             }
 
-            System.out.println("Even number " + number);
-            number++;
+            System.out.println("Even number " + startNumber);
+            startNumber++;
 
             evenNumbersFound++;
             if (evenNumbersFound >= 5) {
